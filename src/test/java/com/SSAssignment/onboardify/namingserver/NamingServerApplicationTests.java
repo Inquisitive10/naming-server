30bfecf40bb64fc20c73600b8560af5c18f7cab2
kumarc <kumarc@opentext.com>
2024-04-28 01:37:40 +0530

    First commit

---
+++ src/test/java/com/SSAssignment/onboardify/namingserver/NamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SSAssignment.onboardify.namingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
